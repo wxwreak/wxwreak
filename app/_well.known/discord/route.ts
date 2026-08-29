30b07b7ccff5a2727c7151c94d65338368cf358d
wpxq <217353155+wpxq@users.noreply.github.com>
2026-08-29 16:56:04 +0200

    feat: removed route.ts & added in public discord verify

--- app/_well.known/discord/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return new NextResponse('dh=626a0f66c1a6abf99f393acc46e730f617f47ee4', {
-    headers: { 'Content-Type': 'text/plain' },
-  });
-}
